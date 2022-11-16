62650e4716bc449d1d9aa070272bcd05c6a3257d
”lundkvistbenjamin” <lundkvistbenjamin@gmail.com>
2022-11-16 10:20:58 +0200

    Added more options

--- app.js
+++ app.js
@@ -1,4 +1,50 @@
-let timer = 30
+const outputElem = document.querySelector("#timer-output")
+const kitchenTimer = null
+
+function readMinSec() {
+    let seconds = document.querySelector("#input-seconds").value
+    if (seconds.includes(":")) {
+        seconds = Number(seconds.split(":")[0]*60) + Number(seconds.split(":")[1])
+    }
+    return seconds
+}
+
+function timerHandler(){
+    if (!kitchenTimer){
+
+        let secondsLeft = readMinSec()
+
+    outputElem.innerText = secondsLeft
+    kitchenTimer = setInterval(function(){
+    secondsLeft--
+
+        let minLeft = Math.floor(secondsLeft/60)
+        outputElem.innerText = `${minLeft}:${secondsLeft % 60}`
+
+    outputElem.innerText = secondsLeft
+
+    if (secondsLeft <= 0){
+        clearInterval(kitchenTimer)
+        outputElem.innerText = "Tiden är ute!"
+    }
+
+}, 1000)
+        
+    } else {
+        clearInterval(kitchenTimer)
+        kitchenTimer = null
+    }
+
+
+
+}
+
+document.querySelector("#btn-start").addEventListener("click", function (){
+    timerHandler()
+})
+
+
+/* //let timer = 30
 const kitchenTimer = setInterval(function() {
     div = document.querySelector("#time")
     timer--
@@ -7,4 +53,4 @@ const kitchenTimer = setInterval(function() {
         div.innerText = "DING DONG"
         clearInterval(kitchenTimer)
     }
-}, 1000)
+}, 1000) */
